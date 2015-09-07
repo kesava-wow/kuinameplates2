79a77fc62c02f5115b8db1a5c408bc0f90df5a1b
kesava <kesava.wow@gmail.com>
2015-09-07 18:19:00 +0100

    and more indents

--- Kui_Nameplates_2/nameplate.lua
+++ Kui_Nameplates_2/nameplate.lua
@@ -18,202 +18,202 @@ addon.Nameplate.__index = addon.Nameplate
 -- BossIcon, Highlight, ThreatGlow
 -- others will either do nothing or are added by external modules
 function addon.Nameplate.RegisterElement(frame, element, element_frame)
-	frame = frame.parent
-	if frame[element] then return end
-	frame.elements[element] = true
-	frame[element] = element_frame
+    frame = frame.parent
+    if frame[element] then return end
+    frame.elements[element] = true
+    frame[element] = element_frame
 end
 function addon.Nameplate.DisableElement(frame, element)
-	frame = frame.parent
-	if frame.elements[element] then
-		frame.elements[element] = false
-	end
+    frame = frame.parent
+    if frame.elements[element] then
+        frame.elements[element] = false
+    end
 end
 function addon.Nameplate.EnableElement(frame, element)
-	frame = frame.parent
-	if frame.elements[element] == false then
-		frame.elements[element] = true
-	end
+    frame = frame.parent
+    if frame.elements[element] == false then
+        frame.elements[element] = true
+    end
 end
 
 ------------------------------------------------------- Frame script handlers --
 function addon.Nameplate.OnShow(f)
-	f = f.parent
-	f.state.name = f.default.nameText:GetText()
-	f.state.level = f.default.levelText:GetText()
-	f.state.micro = f.default.overlayChild:GetScale() < 1
+    f = f.parent
+    f.state.name = f.default.nameText:GetText()
+    f.state.level = f.default.levelText:GetText()
+    f.state.micro = f.default.overlayChild:GetScale() < 1
 
-	if f.default.eliteIcon:IsVisible() then
-		if f.default.eliteIcon:GetTexture() == [[Interface\Tooltips\EliteNameplateIcon]]
-		then
-			f.state.elite = true
-		else
-			f.state.rare = true
-		end
-	end
+    if f.default.eliteIcon:IsVisible() then
+        if f.default.eliteIcon:GetTexture() == [[Interface\Tooltips\EliteNameplateIcon]]
+        then
+            f.state.elite = true
+        else
+            f.state.rare = true
+        end
+    end
 
-	if f.elements.Name then
-		f.Name:SetText(f.state.name)
-	end
+    if f.elements.Name then
+        f.Name:SetText(f.state.name)
+    end
 
-	if f.elements.Level then
-		f.Level:SetText(f.state.level)
-	end
+    if f.elements.Level then
+        f.Level:SetText(f.state.level)
+    end
 
-	if f.elements.BossIcon then
-		f.BossIcon:Show()
-	end
+    if f.elements.BossIcon then
+        f.BossIcon:Show()
+    end
 
-	addon:DispatchMessage('Show', f)
-	f.DoShow = true
+    addon:DispatchMessage('Show', f)
+    f.DoShow = true
 end
 function addon.Nameplate.OnHide(f)
-	f = f.parent
-	f.DoShow = nil
-	f:Hide()
-	addon:DispatchMessage('Hide', f)
+    f = f.parent
+    f.DoShow = nil
+    f:Hide()
+    addon:DispatchMessage('Hide', f)
 
-	-- reset highlight
-	if f.elements.Highlight then
-		f.Highlight:Hide()
-	end
+    -- reset highlight
+    if f.elements.Highlight then
+        f.Highlight:Hide()
+    end
 
-	wipe(f.state)
+    wipe(f.state)
 end
 function addon.Nameplate.Create(f)
-	f = f.parent
-	addon:DispatchMessage('Create', f)
+    f = f.parent
+    addon:DispatchMessage('Create', f)
 end
 
 function addon.Nameplate.OnHealthUpdate(f)
-	f = f.parent
+    f = f.parent
 
-	if f.elements.Healthbar then
-		f.Healthbar:SetMinMaxValues(f.default.healthbar:GetMinMaxValues())
-		f.Healthbar:SetValue(f.default.healthbar:GetValue())
-	end
+    if f.elements.Healthbar then
+        f.Healthbar:SetMinMaxValues(f.default.healthbar:GetMinMaxValues())
+        f.Healthbar:SetValue(f.default.healthbar:GetValue())
+    end
 
-	addon:DispatchMessage('HealthUpdate', f)
+    addon:DispatchMessage('HealthUpdate', f)
 end
 
 function addon.Nameplate.OnCastbarShow(f)
-	f = f.parent
-	f.state.casting = true
+    f = f.parent
+    f.state.casting = true
 
-	if f.elements.Castbar then
-		f.Castbar:SetMinMaxValues(f.default.castbar:GetMinMaxValues())
-		f.Castbar:SetValue(f.default.castbar:GetValue())
-		f.Castbar:Show()
-	end
+    if f.elements.Castbar then
+        f.Castbar:SetMinMaxValues(f.default.castbar:GetMinMaxValues())
+        f.Castbar:SetValue(f.default.castbar:GetValue())
+        f.Castbar:Show()
+    end
 
-	if f.elements.SpellName then
-		f.SpellName:SetText(f.default.spellNameText:GetText())
-	end
+    if f.elements.SpellName then
+        f.SpellName:SetText(f.default.spellNameText:GetText())
+    end
 
-	if f.elements.SpellIcon then
-		f.SpellIcon:SetTexture(f.default.spellIcon:GetTexture())
-	end
+    if f.elements.SpellIcon then
+        f.SpellIcon:SetTexture(f.default.spellIcon:GetTexture())
+    end
 
-	if f.elements.SpellShield then
-		f.SpellShield:Show()
-	end
+    if f.elements.SpellShield then
+        f.SpellShield:Show()
+    end
 
-	addon:DispatchMessage('CastbarShow', f)
+    addon:DispatchMessage('CastbarShow', f)
 end
 function addon.Nameplate.OnCastbarHide(f)
-	f = f.parent
-	f.state.casting = false
+    f = f.parent
+    f.state.casting = false
 
-	if f.elements.Castbar then
-		f.Castbar:Hide()
-	end
+    if f.elements.Castbar then
+        f.Castbar:Hide()
+    end
 
-	if f.elements.SpellShield then
-		f.SpellShield:Hide()
-	end
+    if f.elements.SpellShield then
+        f.SpellShield:Hide()
+    end
 
-	addon:DispatchMessage('CastbarHide', f)
+    addon:DispatchMessage('CastbarHide', f)
 end
 function addon.Nameplate.OnCastbarUpdate(f)
-	if not f.parent.state.casting then return end
-	f = f.parent
+    if not f.parent.state.casting then return end
+    f = f.parent
 
-	if f.elements.Castbar then
-		f.Castbar:SetMinMaxValues(f.default.castbar:GetMinMaxValues())
-		f.Castbar:SetValue(f.default.castbar:GetValue())
-	end
+    if f.elements.Castbar then
+        f.Castbar:SetMinMaxValues(f.default.castbar:GetMinMaxValues())
+        f.Castbar:SetValue(f.default.castbar:GetValue())
+    end
 end
 ------------------------------------------------------------ update functions --
 -- watch for health colour changes
 local function UpdateHealthColour(f)
-	local r,g,b = f.default.healthbar:GetStatusBarColor()
-	if not f.state.healthColour or
-	   f.state.healthColour[1] ~= r or
-	   f.state.healthColour[2] ~= g or
-	   f.state.healthColour[3] ~= b
-	then
-		f.state.healthColour = { r,g,b }
+    local r,g,b = f.default.healthbar:GetStatusBarColor()
+    if not f.state.healthColour or
+       f.state.healthColour[1] ~= r or
+       f.state.healthColour[2] ~= g or
+       f.state.healthColour[3] ~= b
+    then
+        f.state.healthColour = { r,g,b }
 
-		if f.elements.Healthbar then
-			f.Healthbar:SetStatusBarColor(unpack(f.state.healthColour))
-		end
+        if f.elements.Healthbar then
+            f.Healthbar:SetStatusBarColor(unpack(f.state.healthColour))
+        end
 
-		addon:DispatchMessage('HealthColourChange', f)
-	end
+        addon:DispatchMessage('HealthColourChange', f)
+    end
 end
 -- watch for glow colour changes
 local function UpdateGlowColour(f)
-	if f.default.glow:IsShown() then
-		f.state.glowing = true
-		local r,g,b,a = f.default.glow:GetVertexColor()
-		if not f.state.glowColour or
-		   f.state.glowColour[1] ~= r or
-		   f.state.glowColour[2] ~= g or
-		   f.state.glowColour[3] ~= b or
-		   f.state.glowColour[4] ~= a
-		then
-			f.state.glowColour = { r,g,b,a }
+    if f.default.glow:IsShown() then
+        f.state.glowing = true
+        local r,g,b,a = f.default.glow:GetVertexColor()
+        if not f.state.glowColour or
+           f.state.glowColour[1] ~= r or
+           f.state.glowColour[2] ~= g or
+           f.state.glowColour[3] ~= b or
+           f.state.glowColour[4] ~= a
+        then
+            f.state.glowColour = { r,g,b,a }
 
-			if f.elements.ThreatGlow then
-				f.ThreatGlow:SetVertexColor(unpack(f.state.glowColour))
-			end
+            if f.elements.ThreatGlow then
+                f.ThreatGlow:SetVertexColor(unpack(f.state.glowColour))
+            end
 
-			addon:DispatchMessage('GlowColourChange', f)
-		end
-	elseif f.state.glowing or not f.state.glowColour then
-		f.state.glowing = false
-		f.state.glowColour = { 0, 0, 0, 0 }
-		addon:DispatchMessage('GlowColourChange', f)
-	end
+            addon:DispatchMessage('GlowColourChange', f)
+        end
+    elseif f.state.glowing or not f.state.glowColour then
+        f.state.glowing = false
+        f.state.glowColour = { 0, 0, 0, 0 }
+        addon:DispatchMessage('GlowColourChange', f)
+    end
 end
 -- check for mouseover highlight
 local function UpdateMouseover(f)
-	if f.default.highlight:IsShown() then
-		if not f.state.highlight then
-			f.state.highlight = true
+    if f.default.highlight:IsShown() then
+        if not f.state.highlight then
+            f.state.highlight = true
 
-			if f.elements.Highlight then
-				f.Highlight:Show()
-			end
+            if f.elements.Highlight then
+                f.Highlight:Show()
+            end
 
-			addon:DispatchMessage('OnEnter', f)
-		end
-	else
-		if f.state.highlight then
-			f.state.highlight = false
+            addon:DispatchMessage('OnEnter', f)
+        end
+    else
+        if f.state.highlight then
+            f.state.highlight = false
 
-			if f.elements.Highlight then
-				f.Highlight:Hide()
-			end
+            if f.elements.Highlight then
+                f.Highlight:Hide()
+            end
 
-			addon:DispatchMessage('OnLeave', f)
-		end
-	end
+            addon:DispatchMessage('OnLeave', f)
+        end
+    end
 end
 -------------------------------------------------------- frame update handler --
 function addon.Nameplate.Update(f)
-	f = f.parent
-	UpdateHealthColour(f)
-	UpdateGlowColour(f)
-	UpdateMouseover(f)
+    f = f.parent
+    UpdateHealthColour(f)
+    UpdateGlowColour(f)
+    UpdateMouseover(f)
 end
