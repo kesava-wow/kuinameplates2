001c9e1014699e412e1c97f74cda1b849893f69b
kesava <kesava.wow@gmail.com>
2015-09-07 17:59:22 +0100

    indents

--- Kui_Nameplates_2/addon.lua
+++ Kui_Nameplates_2/addon.lua
@@ -20,69 +20,69 @@ local last_plate_update = PLATE_UPDATE_PERIOD
 local width, height = 142, 40
 ------------------------------------------------------------- find nameplates --
 local function IsNameplate(frame)
-	if not frame or not frame.GetName then return end
-	local name = frame:GetName()
-	if name and strfind(name, '^NamePlate%d') then
-		return true
-	end
+    if not frame or not frame.GetName then return end
+    local name = frame:GetName()
+    if name and strfind(name, '^NamePlate%d') then
+        return true
+    end
 end
 local function FindNameplates()
-	local frames = WorldFrame:GetNumChildren()
+    local frames = WorldFrame:GetNumChildren()
 
-	if frames ~= numFrames then
-		numFrames = frames
+    if frames ~= numFrames then
+        numFrames = frames
 
-		local i,f
-		local children = { WorldFrame:GetChildren() }
+        local i,f
+        local children = { WorldFrame:GetChildren() }
 
-		for i = 1, frames do
-			f = select(i, WorldFrame:GetChildren())
-	
-			if f and not f.kui and IsNameplate(f) then
-				addon.HookNameplate(f)
-				frameList[f] = true
-			end
-		end
-	end
+        for i = 1, frames do
+            f = select(i, WorldFrame:GetChildren())
+
+            if f and not f.kui and IsNameplate(f) then
+                addon.HookNameplate(f)
+                frameList[f] = true
+            end
+        end
+    end
 end
 -- call plate update script every PLATE_UPDATE_PERIOD
 local function IteratePlates(elap)
-	last_plate_update = last_plate_update + elap
+    last_plate_update = last_plate_update + elap
 
-	if last_plate_update > PLATE_UPDATE_PERIOD then
-		last_plate_update = 0
+    if last_plate_update > PLATE_UPDATE_PERIOD then
+        last_plate_update = 0
 
-		local f,_
-		for f,_ in pairs(frameList) do
-			if f.kui:IsShown() then
-				f.kui.handler:Update()
-			end
-		end
-	end
+        local f,_
+        for f,_ in pairs(frameList) do
+            if f.kui:IsShown() then
+                f.kui.handler:Update()
+            end
+        end
+    end
 end
 --------------------------------------------------------------------------------
 local function OnUpdate(self,elap)
-	FindNameplates()
-	IteratePlates(elap)
+    FindNameplates()
+    IteratePlates(elap)
 end
 --------------------------------------------------------------------------------
 local function OnEvent(self,event,...)
-	if event ~= 'PLAYER_LOGIN' then return end
-	self.uiscale = UIParent:GetEffectiveScale()
+    if event ~= 'PLAYER_LOGIN' then return end
+    self.uiscale = UIParent:GetEffectiveScale()
 
-	-- get the pixel-perfect width/height of the default, non-trivial frames
-	self.width, self.height = floor(width / self.uiscale), floor(height / self.uiscale)
+    -- get the pixel-perfect width/height of the default, non-trivial frames
+    self.width, self.height = floor(width / self.uiscale), floor(height / self.uiscale)
 
-	if self.layout then
-		-- initialise the registered layout
-		self.layout:Initialise()
-	else
-		-- throw missing layout
-		print('|cff9900ffKui Namemplates|r: A compatible layout was not loaded. You probably forgot to enable Kui Nameplates: Core in your addon list.')
-	end
+    if self.layout then
+        -- initialise the registered layout
+        self.layout:Initialise()
+    else
+        -- throw missing layout
+        print('|cff9900ffKui Namemplates|r: A compatible layout was not loaded. You probably forgot to enable Kui Nameplates: Core in your addon list.')
+    end
 
-	-- begin searching for nameplates
-	self:SetScript('OnUpdate',OnUpdate)
+    -- begin searching for nameplates
+    self:SetScript('OnUpdate',OnUpdate)
 end
 ------------------------------------------- initialise addon scripts & events --
 addon:SetScript('OnEvent',OnEvent)
