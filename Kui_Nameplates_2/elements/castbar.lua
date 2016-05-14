0a5f4bd0b910aff013d018233e08dd896da5e40e
kesava <kesava.wow@gmail.com>
2016-05-14 20:28:55 +0100

    chanelling support. maybe.

--- Kui_Nameplates_2/elements/castbar.lua
+++ Kui_Nameplates_2/elements/castbar.lua
@@ -6,10 +6,15 @@ local function OnCastbarUpdate(f,elapsed)
     f = f.parent
     if not f.state.casting then return end
 
-    f.state.cast_duration = f.state.cast_duration + elapsed
-    f.Castbar:SetValue(f.state.cast_duration)
+    f.cast_state.duration = f.cast_state.duration + elapsed
 
-    if f.state.cast_duration >= f.state.cast_max then
+    if f.cast_state.channelling then
+        f.Castbar:SetValue(f.cast_state.max - f.cast_state.duration)
+    else
+        f.Castbar:SetValue(f.cast_state.duration)
+    end
+
+    if f.cast_state.duration >= f.cast_state.max then
         f.handler:CastbarHide()
     end
 end
@@ -18,20 +23,26 @@ function addon.Nameplate.CastbarShow(f)
     f = f.parent
 
     if f.elements.Castbar then
-        f.Castbar:SetMinMaxValues(0,f.state.cast_max)
-        f.Castbar:SetValue(0)
+        f.Castbar:SetMinMaxValues(0,f.cast_state.max)
+
+        if f.cast_state.channelling then
+            f.Castbar:SetValue(f.cast_state.max)
+        else
+            f.Castbar:SetValue(0)
+        end
+
         f.Castbar:Show()
     end
 
     if f.elements.SpellName then
-        f.SpellName:SetText(f.state.cast_name)
+        f.SpellName:SetText(f.cast_state.name)
     end
 
     if f.elements.SpellIcon then
-        f.SpellIcon:SetTexture(f.state.cast_icon)
+        f.SpellIcon:SetTexture(f.cast_state.icon)
     end
 
-    if f.elements.SpellShield and not f.state.cast_interruptible then
+    if f.elements.SpellShield and not f.cast_state.interruptible then
         f.SpellShield:Show()
     end
 
@@ -63,33 +74,42 @@ function ele.Create(f)
     f.CastbarUpdateFrame = CreateFrame('Frame')
     f.CastbarUpdateFrame:Hide()
     f.CastbarUpdateFrame.parent = f
+    f.cast_state = {}
 end
 -- events ######################################################################
-function ele:UNIT_SPELLCAST_START(event,f,unit)
+local function CastStart(event,f,unit)
     local name,_,text,texture,startTime,endTime,_,_,notInterruptible = UnitCastingInfo(unit)
+
     startTime = startTime / 1000
-    endTime = endTime / 1000
+    endTime   = endTime / 1000
 
     f.state.casting            = true
-    f.state.cast_name          = text
-    f.state.cast_icon          = texture
-    f.state.cast_duration      = GetTime() - startTime
-    f.state.cast_max           = endTime - startTime
-    f.state.cast_interruptible = not notInterruptible
+    f.cast_state.name          = text
+    f.cast_state.icon          = texture
+    f.cast_state.max           = endTime - startTime
+    f.cast_state.interruptible = not notInterruptible
+
+    if event == 'UNIT_SPELLCAST_CHANNEL_START' then
+        f.cast_state.duration = endTime - GetTime()
+        f.cast_state.channelling = true
+    else
+        f.cast_state.duration = GetTime() - startTime
+    end
 
     f.handler:CastbarShow()
 end
 function ele:UNIT_SPELLCAST_STOP(event,f,unit)
+    wipe(f.cast_state)
     f.state.casting = nil
     f.handler:CastbarHide()
 end
 -- register ####################################################################
 ele:RegisterMessage('Create')
 
-ele:RegisterEvent('UNIT_SPELLCAST_START')
+ele:RegisterEvent('UNIT_SPELLCAST_START','CastStart')
 ele:RegisterEvent('UNIT_SPELLCAST_FAILED')
 ele:RegisterEvent('UNIT_SPELLCAST_STOP')
-ele:RegisterEvent('UNIT_SPELLCAST_CHANNEL_START')
+ele:RegisterEvent('UNIT_SPELLCAST_CHANNEL_START','CastStart')
 ele:RegisterEvent('UNIT_SPELLCAST_CHANNEL_STOP')
 ele:RegisterEvent('UNIT_SPELLCAST_CHANNEL_UPDATE')
 ele:RegisterEvent('UNIT_SPELLCAST_INTERRUPTED')
