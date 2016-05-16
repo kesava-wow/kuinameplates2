02bea3453814395ded8e19fae010c37d1a213165
kesava <kesava.wow@gmail.com>
2016-05-16 20:21:07 +0100

    listen for power updates

--- Kui_Nameplates_2/elements/classpowers.lua
+++ Kui_Nameplates_2/elements/classpowers.lua
@@ -15,13 +15,31 @@ local powers = {
 -- prototype additions #########################################################
 -- messages ####################################################################
 -- events ######################################################################
-function ele:Update(event,f,unit)
-    if event == 'UNIT_MAXPOWER' and unit ~= 'player' then return end
+function ele:PowerInit(event)
+    local power_type
+    if type(powers[class]) == 'table' then
+        local spec = GetSpecilization()
+        power_type = powers[class][spec]
+    else
+        power_type = powers[class]
+    end
+
+    if power_type then
+        ele:RegisterEvent('UNIT_MAXPOWER','PowerUpdate')
+        ele:RegisterEvent('UNIT_POWER','PowerUpdate')
+    else
+        ele:UnregisterEvent('UNIT_MAXPOWER')
+        ele:UnregisterEvent('UNIT_POWER')
+    end
+end
+function ele:PowerUpdate(event,f,unit)
+    if unit ~= 'player' then return end
 end
 -- register ####################################################################
 function ele:Initialise()
     class = select(2,UnitClass('player'))
+    if not powers[class] then return end
 
-    ele:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED','Update')
-    ele:RegisterEvent('UNIT_MAXPOWER','Update')
+    ele:RegisterEvent('PLAYER_LOGIN','PowerInit')
+    ele:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED','PowerInit')
 end
