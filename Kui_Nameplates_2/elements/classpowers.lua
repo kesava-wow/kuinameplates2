8a40a9047659424bbd94c2e418e16f1bf8fd38ac
kesava <kesava.wow@gmail.com>
2016-05-16 19:17:25 +0100

    list of class powers

--- Kui_Nameplates_2/elements/classpowers.lua
+++ Kui_Nameplates_2/elements/classpowers.lua
@@ -2,10 +2,25 @@
 local addon = KuiNameplates
 local ele = addon:NewElement('classpowers')
 local class
+-- power types by class/spec
+local powers = {
+    DEATHKNIGHT = SPELL_POWER_RUNES,
+    DRUID       = { [2] = SPELL_POWER_COMBO_POINTS },
+    PALADIN     = { [3] = SPELL_POWER_HOLY_POWER },
+    ROGUE       = SPELL_POWER_COMBO_POINTS,
+    MAGE        = { [1] = SPELL_POWER_ARCANE_CHARGES },
+    MONK        = { [3] = SPELL_POWER_CHI },
+    WARLOCK     = SPELL_POWER_SOUL_SHARDS,
+}
 -- prototype additions #########################################################
 -- messages ####################################################################
 -- events ######################################################################
+function ele:Update()
+end
 -- register ####################################################################
 function ele:Initialise()
     class = select(2,UnitClass('player'))
+
+    ele:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED','Update')
+    ele:RegisterEvent('UNIT_MAXPOWER','Update')
 end
