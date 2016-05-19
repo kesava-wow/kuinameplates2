f2026b872334cd4911a06e796cba7c7758e1f4f0
kesava <kesava.wow@gmail.com>
2016-05-19 01:31:19 +0100

    add RegisterUnitEvent

--- Kui_Nameplates_2/messages.lua
+++ Kui_Nameplates_2/messages.lua
@@ -30,47 +30,46 @@ end
 ----------------------------------------------------------------- event frame --
 local event_frame = CreateFrame('Frame')
 local event_index = {}
-
+-- fire events to listeners
 local function event_frame_OnEvent(self,event,...)
     if not event_index[event] then
         self:UnregisterEvent(event)
         return
     end
 
-    local unit_frame,unit
-    if event:find('UNIT') == 1 then
-        unit = ...
-        unit_frame = C_NamePlate.GetNamePlateForUnit(unit)
-        if unit_frame then
-            unit_frame = unit_frame.kui
-        else
-            unit_frame = nil
-        end
-    end
-
+    local unit,unit_frame
     for i,table_tbl in ipairs(event_index[event]) do
-        local table,func = unpack(table_tbl)
+        local table,func,unit_only = unpack(table_tbl)
 
-        if type(func) == 'string' and type(table[func]) == 'function' then
-            func = table[func]
-        elseif type(table[event]) == 'function' then
-            func = table[event]
+        if unit_only and not unit then
+            -- first unit_only listener; find nameplate
+            unit = ...
+            if unit then
+                unit_frame = C_NamePlate.GetNamePlateForUnit(unit)
+                unit_frame = unit_frame and unit_frame.kui
+            end
         end
 
-        if type(func) == 'function' then
-            if unit and unit_frame then
-                func(table, event, unit_frame, unit, select(2,...))
-            elseif unit then
-                func(table, event, nil, unit, select(2,...))
-            else
-                func(table, event, ...)
+        if not unit_only or unit_frame then
+            if type(func) == 'string' and type(table[func]) == 'function' then
+                func = table[func]
+            elseif type(table[event]) == 'function' then
+                func = table[event]
             end
 
-            if addon.debug_messages then
-                addon:print('event '..event..(unit and ' ['..unit..']' or '')..' > '..(table.name or 'nil'))
+            if type(func) == 'function' then
+                if unit_only then
+                    func(table, event, unit_frame, unit, select(2,...))
+                else
+                    func(table, event, ...)
+                end
+
+                if addon.debug_messages then
+                    addon:print('event '..event..(unit and ' ['..unit..']' or '')..' > '..(table.name or 'nil'))
+                end
+            else
+                addon:print('|cffff0000no event listener for '..event..' in '..(table.name or 'nil'))
             end
-        else
-            addon:print('|cffff0000no event listener for '..event..' in '..(table.name or 'nil'))
         end
     end
 end
@@ -112,12 +111,20 @@ local function pluginHasEvent(table,event)
     -- true if plugin is registered for given event
     return (type(table.__EVENTS) == 'table' and table.__EVENTS[event])
 end
-function message.RegisterEvent(table,event,func)
+function message.RegisterEvent(table,event,func,unit_only)
+    -- unit_only: only fire callback if a valid nameplate exists for event unit
+    if unit_only and event:find('UNIT') ~= 1 then
+        addon:print('|cffff0000unit_only doesn\'t make sense for '..event)
+        return
+    end
+
     if not event_index[event] then
         event_index[event] = {}
     end
 
     if pluginHasEvent(table,event) then return end
+
+    local insert_tbl = { table, func, unit_only }
 
     -- insert by priority
     if #event_index[event] > 0 then
@@ -125,17 +132,17 @@ function message.RegisterEvent(table,event,func)
         for k,listener in ipairs(event_index[event]) do
             listener = listener[1]
             if listener.priority > table.priority then
-                tinsert(event_index[event], k, { table, func })
+                tinsert(event_index[event], k, insert_tbl)
                 inserted = true
                 break
             end
         end
 
         if not inserted then
-            tinsert(event_index[event], { table, func })
+            tinsert(event_index[event], insert_tbl)
         end
     else
-        tinsert(event_index[event], { table, func })
+        tinsert(event_index[event], insert_tbl)
     end
 
     if not table.__EVENTS then
@@ -144,6 +151,9 @@ function message.RegisterEvent(table,event,func)
     table.__EVENTS[event] = true
 
     event_frame:RegisterEvent(event)
+end
+function message.RegisterUnitEvent(table,event,func)
+    table:RegisterEvent(event,func,true)
 end
 function message.UnregisterEvent(table,event)
     if not pluginHasEvent(table,event) then return end
