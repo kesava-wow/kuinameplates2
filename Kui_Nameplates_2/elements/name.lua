279560504015d55b46ddbaec1ab9487496708f5c
kesava <kesava.wow@gmail.com>
2016-05-29 06:01:26 +0100

    add namechange message

--- Kui_Nameplates_2/elements/name.lua
+++ Kui_Nameplates_2/elements/name.lua
@@ -8,6 +8,8 @@ function addon.Nameplate.UpdateName(f)
     if f.elements.NameText then
         f.NameText:SetText(f.state.name)
     end
+
+    addon:DispatchMessage('NameChange',f)
 end
 -- messages ####################################################################
 function ele:Show(f)
