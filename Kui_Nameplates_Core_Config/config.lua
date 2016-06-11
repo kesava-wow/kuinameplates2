fdfed90ab1855a67fa38971aec28b5f218f9576e
kesava <kesava.wow@gmail.com>
2016-06-11 20:44:54 +0100

    add config tabs (#9)

--- Kui_Nameplates_Core_Config/config.lua
+++ Kui_Nameplates_Core_Config/config.lua
@@ -7,6 +7,7 @@
 --------------------------------------------------------------------------------
 local folder,ns = ...
 local category = 'Kui |cff9966ffNameplates Core'
+local frame_name = 'KuiNameplatesCoreConfig'
 local knp = KuiNameplates
 local kc = LibStub('KuiConfig-1.0')
 local config,profile
@@ -28,74 +29,172 @@ if AddonLoader and AddonLoader.RemoveInterfaceOptions then
     hash_SlashCmdList["/kuinameplates"] = nil
     hash_SlashCmdList["/knp"] = nil
 end
--- #############################################################################
-local opt = CreateFrame('Frame','KuiNameplatesCoreConfig',InterfaceOptionsFramePanelContainer)
+-- category container ##########################################################
+local opt = CreateFrame('Frame',frame_name,InterfaceOptionsFramePanelContainer)
 opt:Hide()
 opt.name = category
+opt.pages = {}
 -- helpers #####################################################################
-local function OnEnter(self)
-    GameTooltip:SetOwner(self,'ANCHOR_TOPLEFT')
-    GameTooltip:SetWidth(200)
-    GameTooltip:AddLine(self.desc:GetText())
+do
+    local function OnEnter(self)
+        GameTooltip:SetOwner(self,'ANCHOR_TOPLEFT')
+        GameTooltip:SetWidth(200)
+        GameTooltip:AddLine(self.desc:GetText())
 
-    if tooltip_text[self.env] then
-        GameTooltip:AddLine(tooltip_text[self.env], 1,1,1,true)
+        if tooltip_text[self.env] then
+            GameTooltip:AddLine(tooltip_text[self.env], 1,1,1,true)
+        end
+
+        GameTooltip:Show()
     end
-
-    GameTooltip:Show()
-end
-local function OnLeave(self)
-    GameTooltip:Hide()
-end
-local function CheckBoxOnClick(self)
-    if self:GetChecked() then
-        PlaySound("igMainMenuOptionCheckBoxOn")
-    else
-        PlaySound("igMainMenuOptionCheckBoxOff")
+    local function OnLeave(self)
+        GameTooltip:Hide()
     end
+    local function CheckBoxOnClick(self)
+        if self:GetChecked() then
+            PlaySound("igMainMenuOptionCheckBoxOn")
+        else
+            PlaySound("igMainMenuOptionCheckBoxOff")
+        end
 
-    if self.env then
-        config:SetConfig(self.env,self:GetChecked())
+        if self.env then
+            config:SetConfig(self.env,self:GetChecked())
+        end
+
+        if self.callback then
+            self:callback()
+        end
     end
+    local function CheckBoxOnShow(self)
+        if not profile then return end
+        if self.env then
+            self:SetChecked(profile[self.env])
+        end
+    end
+    local function CreateCheckBox(parent, name, desc, callback)
+        local check = CreateFrame('CheckButton', frame_name..name..'Check', parent, 'OptionsBaseCheckButtonTemplate')
 
-    if self.callback then
-        self:callback()
+        check.env = name
+        check.callback = callback
+        check:SetScript('OnClick',CheckBoxOnClick)
+        check:SetScript('OnShow',CheckBoxOnShow)
+
+        check:HookScript('OnEnter',OnEnter)
+        check:HookScript('OnLeave',OnLeave)
+
+        check.desc = parent:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
+        check.desc:SetText(desc)
+        check.desc:SetPoint('LEFT', check, 'RIGHT')
+
+        return check
+    end
+    -- page functions
+    local function ShowPage(self)
+        if opt.active_page then
+            opt.active_page:HidePage()
+        end
+
+        self.scroll:Show()
+        self.bg:Show()
+        self:Show()
+
+        opt.active_page = self
+    end
+    local function HidePage(self)
+        self.scroll:Hide()
+        self.bg:Hide()
+        self:Hide()
+    end
+    local page_proto = {
+        CreateCheckBox = CreateCheckBox,
+
+        HidePage = HidePage,
+        ShowPage = ShowPage
+    }
+    function opt:CreateConfigPage(name)
+        local f = CreateFrame('Frame',frame_name..name..'Page',self)
+        f.name = name
+
+        f.scroll = CreateFrame('ScrollFrame',frame_name..name..'PageScrollFrame',self,'UIPanelScrollFrameTemplate')
+        f.scroll:SetPoint('TOPLEFT',20,-50)
+        f.scroll:SetPoint('BOTTOMRIGHT',-40,20)
+        f.scroll:SetScrollChild(f)
+
+        f.bg = CreateFrame('Frame',nil,self)
+        f.bg:SetBackdrop({
+            bgFile = 'Interface/ChatFrame/ChatFrameBackground',
+            edgeFile = 'Interface/Tooltips/UI-Tooltip-border',
+            edgeSize = 16,
+            insets = { left = 4, right = 4, top = 4, bottom = 4 }
+        })
+        f.bg:SetBackdropColor(.1, .1, .1, .3)
+        f.bg:SetBackdropBorderColor(.5, .5, .5)
+        f.bg:SetPoint('TOPLEFT',f.scroll,-10,10)
+        f.bg:SetPoint('BOTTOMRIGHT',f.scroll,30,-10)
+
+        f:SetWidth(1)
+        f:SetHeight(1000)
+
+        -- mixin page functions
+        for k,v in pairs(page_proto) do
+            f[k]=v
+        end
+
+        f:HidePage()
+
+        tinsert(self.pages,f)
+        return f
     end
 end
-local function CheckBoxOnShow(self)
-    if not profile then return end
-    if self.env then
-        self:SetChecked(profile[self.env])
+-- tab functions ###############################################################
+do
+    local function OnClick(self)
+        self.child:ShowPage()
+    end
+    function opt:CreateTabs(self)
+        local pt
+        for k,v in ipairs(opt.pages) do
+            local tab = CreateFrame('Button',frame_name..v.name..'PageTab',opt,'TabButtonTemplate')
+            tab:HookScript('OnClick',OnClick)
+            tab:SetText(v.name)
+            tab.child = v
+
+            if pt then
+                tab:SetPoint('LEFT',pt,'RIGHT')
+            else
+                tab:SetPoint('TOPLEFT',10,-8)
+            end
+
+            PanelTemplates_TabResize(tab,5)
+
+            pt = tab
+        end
+
     end
 end
-local function CreateCheckBox(name, desc, callback)
-    local check = CreateFrame('CheckButton', 'KuiNameplatesCoreConfig'..name..'Check', opt, 'OptionsBaseCheckButtonTemplate')
-
-    check.env = name
-    check.callback = callback
-    check:SetScript('OnClick',CheckBoxOnClick)
-    check:SetScript('OnShow',CheckBoxOnShow)
-
-    check:HookScript('OnEnter',OnEnter)
-    check:HookScript('OnLeave',OnLeave)
-
-    check.desc = opt:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
-    check.desc:SetText(desc)
-    check.desc:SetPoint('LEFT', check, 'RIGHT')
-
-    return check
-end
+-- #############################################################################
+-- options frame elements ######################################################
+-- page containers #############################################################
+local general = opt:CreateConfigPage('General')
+local test1 = opt:CreateConfigPage('Test 1')
+local test2 = opt:CreateConfigPage('Test 2')
+local test3 = opt:CreateConfigPage('Test 3')
 -- create elements #############################################################
-local nameonlyCheck = CreateCheckBox('nameonly','Use nameonly mode')
-local hidenamesCheck = CreateCheckBox('hide_names','Hide unimportant unit names')
-local tankmodeCheck = CreateCheckBox('tank_mode','Enable tank mode')
-local threatbracketsCheck = CreateCheckBox('threat_brackets','Show threat brackets')
+local nameonlyCheck = general:CreateCheckBox('nameonly','Use nameonly mode')
+local hidenamesCheck = general:CreateCheckBox('hide_names','Hide unimportant unit names')
+local tankmodeCheck = general:CreateCheckBox('tank_mode','Enable tank mode')
+local threatbracketsCheck = general:CreateCheckBox('threat_brackets','Show threat brackets')
 
-nameonlyCheck:SetPoint('TOPLEFT',20,-20)
+nameonlyCheck:SetPoint('TOPLEFT',10,-10)
 hidenamesCheck:SetPoint('TOPLEFT',nameonlyCheck,'BOTTOMLEFT')
 tankmodeCheck:SetPoint('TOPLEFT',hidenamesCheck,'BOTTOMLEFT')
 threatbracketsCheck:SetPoint('TOPLEFT',tankmodeCheck,'BOTTOMLEFT')
--- add to interface ############################################################
+-- #############################################################################
+-- create tabs
+opt:CreateTabs()
+-- show inital page
+opt.pages[1]:ShowPage()
+-- add to interface
 InterfaceOptions_AddCategory(opt)
 
 -- 6.2.2: workaround for the category not populating correctly OnClick
