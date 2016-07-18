44ffd7b6e2d2239bc4d7d56ba771d23d89cf92d0
kesava <kesava.wow@gmail.com>
2016-07-18 14:59:17 +0100

    fix dropdown is below some elements

--- Kui_Nameplates_Core_Config/helpers.lua
+++ Kui_Nameplates_Core_Config/helpers.lua
@@ -124,6 +124,7 @@ do
             parent,
             opt.titles[name] or name or 'DropDown'
         )
+        dd:SetFrameStrata('TOOLTIP')
         dd.env = name
 
         dd:HookScript('OnShow',DropDownOnShow)
