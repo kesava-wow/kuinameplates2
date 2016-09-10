8b83f7a3e92fedc6dcbe74019531e0f170666f1b
kesava <kesava.wow@gmail.com>
2016-09-10 19:23:45 +0100

    add roboto to LSM

--- Kui_Nameplates_Core/config.lua
+++ Kui_Nameplates_Core/config.lua
@@ -14,6 +14,7 @@ local cc = CreateFrame('Frame')
 -- add media to LSM ############################################################
 LSM:Register(LSM.MediaType.FONT,'Yanone Kaffesatz Bold',kui.m.f.yanone)
 LSM:Register(LSM.MediaType.FONT,'FrancoisOne',kui.m.f.francois)
+LSM:Register(LSM.MediaType.FONT,'Roboto Condensed Bold',kui.m.f.roboto)
 
 LSM:Register(LSM.MediaType.STATUSBAR, 'Kui status bar', kui.m.t.bar)
 LSM:Register(LSM.MediaType.STATUSBAR, 'Kui shaded bar', kui.m.t.oldbar)
