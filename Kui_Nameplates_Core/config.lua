ff5f9049d03440b0d6a8db1ae13e47c9b64b1be4
kesava <kesava.wow@gmail.com>
2016-09-16 01:38:12 +0100

    increase default icon size

--- Kui_Nameplates_Core/config.lua
+++ Kui_Nameplates_Core/config.lua
@@ -133,7 +133,7 @@ local default_config = {
 
     classpowers_enable = true,
     classpowers_on_target = true,
-    classpowers_size = 10,
+    classpowers_size = 11,
     classpowers_bar_width = 50,
     classpowers_bar_height = 3,
 
