6d4cd97d332ec1be288cd5b2db5929ed46a40ffd
kesava <kesava.wow@gmail.com>
2017-02-05 15:13:52 +0000

    show/hide aura by guid, register dbm callbacks

--- Kui_Nameplates/plugins/bossmods.lua
+++ Kui_Nameplates/plugins/bossmods.lua
@@ -6,6 +6,15 @@ local mod = addon:NewPlugin('BossMods')
 local active_boss_auras
 local plugin_ct
 
+-- local functions #############################################################
+local function GetFrameByGUID(guid)
+    -- TODO store guids => frames OnShow/Hide
+    for k,f in addon:Frames() do
+        if f:IsShown() and UnitGUID(f.unit) == guid then
+            return f
+        end
+    end
+end
 -- callbacks ###################################################################
 do
     local prev_val
@@ -27,14 +36,21 @@ do
             -- TODO won't restore to previous setting if this is triggered
             -- during combat + CombatToggle isn't set up
         end
+
+        -- we're assuming this is out of combat after the end of a boss, so we
+        -- can also use it to clear all auras
+        active_boss_auras = nil
+
+        for k,f in addon:Frames() do
+            if f:IsShown() then
+                f.BossModIcon:Hide()
+            end
+        end
     end
 end
 
-function mod:BigWigs_ShowNameplateAura(unit,icon)
-    -- store icon by guid so we can show/hide icons if the frame isn't
-    -- already visible
-    local guid = UnitGUID(unit)
-
+function mod:BigWigs_ShowNameplateAura(guid,icon,duration)
+    -- store icon so we can show/hide icons if the frame isn't already visible
     if not active_boss_auras then
         active_boss_auras = {}
     end
@@ -42,23 +58,22 @@ function mod:BigWigs_ShowNameplateAura(unit,icon)
     active_boss_auras[guid] = icon
 
     -- immediately show if they already have a frame
-    local f = C_NamePlate.GetNamePlateForUnit(unit)
-    if f and f.kui and f.kui.BossModIcon then
-        f.kui.BossModIcon:SetTexture(icon)
-        f.kui.BossModIcon:Show()
+    local f = GetFrameByGUID(guid)
+    if f and f.BossModIcon then
+        f.BossModIcon:SetTexture(icon)
+        f.BossModIcon:Show()
     end
 end
-function mod:BigWigs_HideNameplateAura(unit)
+function mod:BigWigs_HideNameplateAura(guid)
     if active_boss_auras then
         -- remove from guid list
-        local guid = UnitGUID(unit)
         active_boss_auras[guid] = nil
     end
 
     -- immediately hide
-    local f = C_NamePlate.GetNamePlateForUnit(unit)
-    if f and f.kui and f.kui.BossModIcon then
-        f.kui.BossModIcon:Hide()
+    local f = GetFrameByGUID(guid)
+    if f and f.BossModIcon then
+        f.BossModIcon:Hide()
     end
 end
 -- messages ####################################################################
@@ -95,17 +110,36 @@ end
 -- register ####################################################################
 function mod:OnEnable()
     local BigWigsLoader = BigWigsLoader
-    if BigWigsLoader then
+    local DBM = DBM
+    if BigWigsLoader or DBM then
         self:RegisterMessage('Show')
         self:RegisterMessage('Hide')
         self:RegisterMessage('Create')
 
         plugin_ct = addon:GetPlugin('CombatToggle')
 
-        BigWigsLoader.RegisterMessage(mod,'BigWigs_EnableFriendlyNameplates')
-        BigWigsLoader.RegisterMessage(mod,'BigWigs_DisableFriendlyNameplates')
+        if BigWigsLoader then
+            BigWigsLoader.RegisterMessage(mod,'BigWigs_EnableFriendlyNameplates')
+            BigWigsLoader.RegisterMessage(mod,'BigWigs_DisableFriendlyNameplates')
 
-        BigWigsLoader.RegisterMessage(mod,'BigWigs_ShowNameplateAura')
-        BigWigsLoader.RegisterMessage(mod,'BigWigs_HideNameplateAura')
+            BigWigsLoader.RegisterMessage(mod,'BigWigs_ShowNameplateAura')
+            BigWigsLoader.RegisterMessage(mod,'BigWigs_HideNameplateAura')
+        end
+
+        if DBM then
+            DBM:RegisterCallback('BossMod_EnableFriendlyNameplates',function()
+                mod:BigWigs_EnableFriendlyNameplates()
+            end)
+            DBM:RegisterCallback('BossMod_DisableFriendlyNameplates',function()
+                mod:BigWigs_DisableFriendlyNameplates()
+            end)
+
+            DBM:RegisterCallback('BossMod_ShowNameplateAura',function(msg,guid,texture,duration)
+                mod:BigWigs_ShowNameplateAura(guid,texture,duration)
+            end)
+            DBM:RegisterCallback('BossMod_HideNameplateAura',function(msg,guid,texture)
+                mod:BigWigs_HideNameplateAura(guid)
+            end)
+        end
     end
 end
