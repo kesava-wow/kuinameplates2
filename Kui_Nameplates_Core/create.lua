7926b9a7da9b659b7570f2c1b480d11f49051e69
kesava <kesava.wow@gmail.com>
2017-06-25 05:16:16 +0100

    fix spell icon size is incorrect on newly created frames

--- Kui_Nameplates_Core/create.lua
+++ Kui_Nameplates_Core/create.lua
@@ -914,10 +914,10 @@ end
 -- castbar #####################################################################
 do
     local function SpellIconSetWidth(f)
-        -- set spell icon width (based on height)
-        -- this seems to convince it to calculate the actual height
-        f.SpellIcon.bg:SetHeight(1)
-        f.SpellIcon.bg:SetWidth(floor(f.SpellIcon.bg:GetHeight()*1.25))
+        -- set spell icon width (as it's based on height)
+        if f.SpellIcon.bg:IsShown() then
+            f.SpellIcon.bg:SetWidth(ceil(((f.CastBar.bg:GetHeight() + f.bg:GetHeight() + 1)*1.25)+.1))
+        end
     end
     local function ShowCastBar(f)
         if not f.elements.CastBar then
