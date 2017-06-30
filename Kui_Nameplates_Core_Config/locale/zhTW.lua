1d4112e0db841fc047bec3c17d874a3beeed6abe
EKE00372 <eke00372@gmail.com>
2017-07-01 07:52:22 +0800

    Create zhTW.lua

---
+++ Kui_Nameplates_Core_Config/locale/zhTW.lua
@@ -0,0 +1,13 @@
+if GetLocale() ~= 'zhTW' then return end
+local opt = KuiNameplatesCoreConfig
+
+opt.page_names["general"] = "一般"
+opt.page_names["healthbars "] = "血條"
+opt.page_names["castbars"] = "施法條"
+opt.page_names["text"] = "文字"
+opt.page_names["nameonly"] = "名字模式"
+opt.page_names["framesizes"] = "框架尺寸"
+opt.page_names["auras"] = "光環"
+opt.page_names["classpowers"] = "職業資源"
+opt.page_names["threat"] = "威脅"
+opt.page_names["bossmod"] = "首領模組"
