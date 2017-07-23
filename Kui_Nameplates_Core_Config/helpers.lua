1ce8742b0bdefa0c756cc6593c29d58f8594b4dd
kesava <kesava.wow@gmail.com>
2017-07-23 19:37:54 +0100

    remove manual dropdown strata fix

--- Kui_Nameplates_Core_Config/helpers.lua
+++ Kui_Nameplates_Core_Config/helpers.lua
@@ -149,12 +149,6 @@ do
         self:Get()
         GenericOnShow(self)
     end
-    local function DropDownOnClick(self)
-        -- fix dropdown list frame strata
-        if self:GetParent() and self:GetParent().list then
-            self:GetParent().list:SetFrameStrata('TOOLTIP')
-        end
-    end
 
     local function DropDownEnable(self)
         self.labelText:SetFontObject('GameFontNormalSmall')
@@ -178,7 +172,6 @@ do
         dd.env = name
 
         dd:HookScript('OnShow',DropDownOnShow)
-        dd.button:HookScript('OnClick',DropDownOnClick)
 
         dd.OnEnter = OnEnter
         dd.OnLeave = OnLeave
