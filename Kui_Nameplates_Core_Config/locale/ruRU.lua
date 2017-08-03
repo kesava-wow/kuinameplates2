01a428657f4191d423fb2e1442f1d5c181f42b16
kesava <kesava.wow@gmail.com>
2017-08-03 10:14:49 +0100

    whoops

--- Kui_Nameplates_Core_Config/locale/ruRU.lua
+++ Kui_Nameplates_Core_Config/locale/ruRU.lua
@@ -1,4 +1,4 @@
---if GetLocale() ~= 'ruRU' then return end
+if GetLocale() ~= 'ruRU' then return end
 local opt = KuiNameplatesCoreConfig
 opt.page_names["auras"] = "Ауры"
 opt.page_names["bossmod"] = "Босс моды"
