c05e2a03c22002a4fd477b1653ef4d8dbc2ad487
kesava <kesava.wow@gmail.com>
2018-04-26 19:38:15 +0100

    set name colour npc defaults

--- Kui_Nameplates_Core/config.lua
+++ Kui_Nameplates_Core/config.lua
@@ -89,9 +89,9 @@ local default_config = {
     name_colour_player_friendly = {.6,.7,1},
     name_colour_player_hostile  = {1,0,0},
     name_colour_npcs_inherit_reaction = false,
-    name_colour_npc_friendly = {1,1,1},
-    name_colour_npc_neutral = {1,1,1},
-    name_colour_npc_hostile = {1,1,1},
+    name_colour_npc_friendly = {1,.7,.6},
+    name_colour_npc_neutral = {1,.9,.7},
+    name_colour_npc_hostile = {.8,1,.8},
 
     health_text_friend_max = 5,
     health_text_friend_dmg = 4,
