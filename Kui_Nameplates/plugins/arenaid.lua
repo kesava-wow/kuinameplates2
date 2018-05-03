5accdc00a52bf445f2fd021dc1d6d3b37b282765
kesava <kesava.wow@gmail.com>
2018-05-03 22:45:58 +0100

    fix arenaid

--- Kui_Nameplates/plugins/arenaid.lua
+++ Kui_Nameplates/plugins/arenaid.lua
@@ -4,14 +4,14 @@ local kui = LibStub('Kui-1.0')
 local mod = addon:NewPlugin('ArenaID')
 
 local in_arena
-local UnitIsUnit,IsActiveBattlefieldArena,GetNumArenaOpponents =
-      UnitIsUnit,IsActiveBattlefieldArena,GetNumArenaOpponents
+local IsInInstance,UnitIsUnit,GetNumArenaOpponents =
+      IsInInstance,UnitIsUnit,GetNumArenaOpponents
 
 -- local functions #############################################################
 local function GetArenaID(unit)
     for i=1,GetNumArenaOpponents() do
-        if  UnitIsUnit(f.unit,'arena'..i) or
-            UnitIsUnit(f.unit,'arenapet'..i)
+        if  UnitIsUnit(unit,'arena'..i) or
+            UnitIsUnit(unit,'arenapet'..i)
         then
             return i
         end
@@ -25,7 +25,8 @@ function mod:Show(f)
 end
 -- events ######################################################################
 function mod:PLAYER_ENTERING_WORLD()
-    in_arena = IsActiveBattlefieldArena()
+    local in_instance,instance_type = IsInInstance()
+    in_arena = in_instance and instance_type == 'arena'
 end
 -- register ####################################################################
 function mod:OnEnable()
