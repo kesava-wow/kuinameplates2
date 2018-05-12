485444267019e671310f972d6d9e09e2379571b5
kesava <kesava.wow@gmail.com>
2018-05-12 18:25:13 +0100

    merge master which it made me manually resolve for some reason

--- Kui_Nameplates_Core/config.lua
+++ Kui_Nameplates_Core/config.lua
@@ -72,6 +72,7 @@ local default_config = {
     fade_avoid_execute_friend = false,
     fade_avoid_execute_hostile = false,
     fade_avoid_tracked = false,
+    fade_avoid_combat = false,
     fade_avoid_casting_friendly = false,
     fade_avoid_casting_hostile = false,
     fade_avoid_casting_interruptible = false,
@@ -334,6 +335,12 @@ local function configChangedFadeRule(v,on_load)
         end,22)
     end
 
+    if core.profile.fade_avoid_combat then
+        plugin:AddFadeRule(function(f)
+            return UnitAffectingCombat(f.unit) and 1
+        end,23)
+    end
+
     if core.profile.fade_avoid_casting_interruptible or
        core.profile.fade_avoid_casting_uninterruptible
     then
@@ -362,7 +369,7 @@ local function configChangedFadeRule(v,on_load)
             if f.state.casting then
                 return FadeRule_Casting_CanAttack(f)
             end
-        end,23)
+        end,24)
     end
 
     if core.profile.fade_neutral_enemy then
@@ -395,6 +402,7 @@ configChanged.fade_avoid_raidicon = configChangedFadeRule
 configChanged.fade_avoid_execute_friend = configChangedFadeRule
 configChanged.fade_avoid_execute_hostile = configChangedFadeRule
 configChanged.fade_avoid_tracked = configChangedFadeRule
+configChanged.fade_avoid_combat = configChangedFadeRule
 configChanged.fade_avoid_casting_friendly = configChangedFadeRule
 configChanged.fade_avoid_casting_hostile = configChangedFadeRule
 configChanged.fade_avoid_casting_interruptible = configChangedFadeRule
@@ -783,7 +791,7 @@ configLoaded.bossmod_enable = configChanged.bossmod_enable
 -- init config #################################################################
 function core:InitialiseConfig()
     -- XXX 2.15>2.16 health display transition
-    if not KuiNameplatesCoreSaved['216_HEALTH_TRANSITION'] then
+    if KuiNameplatesCoreSaved and not KuiNameplatesCoreSaved['216_HEALTH_TRANSITION'] then
         KuiNameplatesCoreSaved['216_HEALTH_TRANSITION'] = true
         -- re-jigger health display patterns on all profiles (where set)
         local upd = function(n,k)
--- Kui_Nameplates_Core/create.lua
+++ Kui_Nameplates_Core/create.lua
@@ -56,7 +56,7 @@ local FONT,FONT_STYLE,FONT_SHADOW,FONT_SIZE_NORMAL,FONT_SIZE_SMALL
 local TEXT_VERTICAL_OFFSET,NAME_VERTICAL_OFFSET,BOT_VERTICAL_OFFSET
 local BAR_TEXTURE,BAR_ANIMATION,SHOW_STATE_ICONS
 local FADE_AVOID_NAMEONLY,FADE_UNTRACKED,FADE_AVOID_TRACKED
-local FADE_AVOID_CASTING
+local FADE_AVOID_COMBAT,FADE_AVOID_CASTING
 local SHOW_HEALTH_TEXT,SHOW_NAME_TEXT,SHOW_ARENA_ID
 local GUILD_TEXT_NPCS,GUILD_TEXT_PLAYERS,TITLE_TEXT_PLAYERS
 local HEALTH_TEXT_FRIEND_MAX,HEALTH_TEXT_FRIEND_DMG
@@ -232,6 +232,7 @@ do
         FADE_AVOID_NAMEONLY = self.profile.fade_avoid_nameonly
         FADE_UNTRACKED = self.profile.fade_untracked
         FADE_AVOID_TRACKED = self.profile.fade_avoid_tracked
+        FADE_AVOID_COMBAT = self.profile.fade_avoid_combat
         FADE_AVOID_CASTING =
             (self.profile.fade_avoid_casting_friendly
             or self.profile.fade_avoid_casting_hostile) and
@@ -1597,8 +1598,10 @@ end
 function core:ShowNameUpdate(f)
     if  not FADE_UNTRACKED and
         not FADE_AVOID_TRACKED and
+        not FADE_AVOID_COMBAT and
         f.IN_NAMEONLY
     then
+        -- no need to calculate when in name-only (unless fade rules are used)
         return
     end
 
@@ -1619,7 +1622,7 @@ function core:ShowNameUpdate(f)
         f.state.no_name = true
     end
 
-    if FADE_UNTRACKED or FADE_AVOID_TRACKED then
+    if FADE_UNTRACKED or FADE_AVOID_TRACKED or FADE_AVOID_COMBAT then
         plugin_fading:UpdateFrame(f)
     end
 end
