3d36d40a7018e32961e89f1453816a5ce18d0c6e
kesava <kesava.wow@gmail.com>
2018-09-09 04:11:12 +0100

    add CombatUpdate

---
+++ Kui_Nameplates/plugins/combat-update.lua
@@ -0,0 +1,54 @@
+-- provides f.state.combat, f.state.attackable
+-- messages: FactionUpdate, Combat
+local addon = KuiNameplates
+local kui = LibStub('Kui-1.0')
+local mod = addon:NewPlugin('CombatUpdate')
+
+local INTERVAL = 1
+local elapsed = 0
+
+-- local functions #############################################################
+local function Frame_UpdateCombat(f)
+    f.state.combat = UnitAffectingCombat(f.unit)
+end
+local function Frame_UpdateAttackable(f)
+    f.state.attackable = UnitCanAttack('player',f.unit)
+end
+local function Frame_Check(f)
+    if f.state.combat ~= UnitAffectingCombat(f.unit) then
+        Frame_UpdateCombat(f)
+        addon:DispatchMessage('Combat',f)
+    end
+    if f.state.attackable ~= UnitCanAttack('player',f.unit) then
+        Frame_UpdateAttackable(f)
+        addon:DispatchMessage('FactionUpdate',f)
+    end
+end
+local function UpdateFrame_OnUpdate(self,elap)
+    elapsed = elapsed + elap
+    if elapsed > INTERVAL then
+        for k,f in addon:Frames() do
+            if f.unit then
+                Frame_Check(f)
+            end
+        end
+        elapsed = 0
+    end
+end
+-- messages ####################################################################
+function mod:Show(f)
+    Frame_UpdateCombat(f)
+    Frame_UpdateAttackable(f)
+end
+-- events ######################################################################
+function mod:UNIT_FACTION(event,f)
+    Frame_Check(f)
+end
+-- register ####################################################################
+function mod:OnEnable()
+    self:RegisterUnitEvent('UNIT_FACTION')
+
+    local f = CreateFrame('Frame')
+    f:SetScript('OnUpdate',UpdateFrame_OnUpdate)
+    self.UpdateFrame = f
+end
