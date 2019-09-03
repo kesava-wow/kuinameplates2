dbb8de4608524352dead1b97bde8bdc7a896d6e5
kesava <kesava.wow@gmail.com>
2019-09-03 03:29:04 +0100

    be more aggressive with threatlib updates

--- Kui_Nameplates/elements/threat.lua
+++ Kui_Nameplates/elements/threat.lua
@@ -63,9 +63,8 @@ function ele:UNIT_THREAT_LIST_UPDATE(_,f,unit)
     end
 end
 -- threat lib callback #########################################################
-local function ThreatLib_ThreatUpdated(_,unit_guid,target_guid)
-    if not unit_guid or not target_guid then return end
-    if unit_guid ~= player_guid then return end
+local function ThreatLib_ThreatUpdated(_,_,target_guid)
+    if not target_guid then return end
 
     local f = addon:GetNameplateForGuid(target_guid)
     if f and f.unit then
@@ -78,7 +77,6 @@ function ele:Initialise()
         ThreatLib = LibStub('ThreatClassic-1.0')
         if not ThreatLib then return end
 
-        local player_guid = UnitGUID('player')
         UnitThreatSituation = function(...)
             return ThreatLib:UnitThreatSituation(...)
         end
