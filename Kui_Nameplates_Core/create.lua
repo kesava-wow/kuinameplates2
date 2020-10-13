a7cc910b294fc5f7ebb0ad7c920f0990ff65ab25
kesava-wow <9901797+kesava-wow@users.noreply.github.com>
2020-10-13 16:15:34 +0100

    revert fix mouseover glow target etc

--- Kui_Nameplates_Core/create.lua
+++ Kui_Nameplates_Core/create.lua
@@ -890,7 +890,7 @@ do
                 f.ThreatGlow:SetVertexColor(unpack(TARGET_GLOW_COLOUR))
                 f.TargetGlow:SetVertexColor(unpack(TARGET_GLOW_COLOUR))
                 f.TargetGlow:Show()
-            elseif MOUSEOVER_GLOW and f.state.highlight and not f.state.target then
+            elseif MOUSEOVER_GLOW and f.state.highlight then
                 -- mouseover glow
                 f.ThreatGlow:SetVertexColor(unpack(MOUSEOVER_GLOW_COLOUR))
                 f.TargetGlow:SetVertexColor(unpack(MOUSEOVER_GLOW_COLOUR))
